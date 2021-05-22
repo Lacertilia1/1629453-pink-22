fa0f84cbbc9da05c8a2427fa5580a679e32bda51
Pavel Ivanov <skyteno69@mail.ru>
2021-05-23 01:16:23 +0300

    Попытка 2

--- source/js/header.js
+++ source/js/header.js
@@ -3,6 +3,7 @@ var navList = document.querySelector(".page-header__list");
 var navToggle = document.querySelector(".page-header__toggle");
 var navToggleItemFirst = document.querySelector(".page-header__toggle-item--first");
 var navToggleItemSecond = document.querySelector(".page-header__toggle-item--second");
+var introHand = document.querySelector(".intro__container");
 
 // Без javascript меню находится в открытом состоянии.
 // Строки ниже закрывают его, чтобы появился "бургер".
@@ -10,6 +11,7 @@ var navToggleItemSecond = document.querySelector(".page-header__toggle-item--sec
 pageHeader.classList.add("page-header--background-js");
 navList.classList.add("page-header__list--hidden");
 navToggle.classList.remove("page-header__toggle--no-js");
+introHand.classList.add("intro__container--opened-menu");
 
 // Функция
 
@@ -19,10 +21,12 @@ navToggle.addEventListener("click", function () {
     navList.classList.remove("page-header__list--hidden");
     navToggleItemFirst.classList.add("page-header__toggle-item--hidden");
     navToggleItemSecond.classList.remove("page-header__toggle-item--hidden");
+    introHand.classList.remove("intro__container--opened-menu");
   } else {
     pageHeader.classList.add("page-header--background-js");
     navList.classList.add("page-header__list--hidden");
     navToggleItemFirst.classList.remove("page-header__toggle-item--hidden");
     navToggleItemSecond.classList.add("page-header__toggle-item--hidden");
+    introHand.classList.add("intro__container--opened-menu");
   }
 });
